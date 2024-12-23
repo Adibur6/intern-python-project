4c639d815713230ac74ed2039c27dc8b7a85a59f
Ashfaqur Rahman <ashfaqur.rahman@optimizely.com>
2024-12-24 03:25:46 +0600

    refactor TwitterRequestHandler methods to accept headers and implement retry mechanism for Twitter class

--- util.py
+++ util.py
@@ -50,44 +50,44 @@ class TwitterRequestHandler:
         return f"{cls.base_url}{endpoint}"
 
     @classmethod
-    def list_users(cls):
-        return NetworkRequest.get(cls._get_url('/users'))
+    def list_users(cls, headers={}):
+        return NetworkRequest.get(cls._get_url('/users'), headers=headers)
 
     @classmethod
-    def create_user(cls, data):
-        return NetworkRequest.post(cls._get_url('/users'), data)
+    def create_user(cls, data, headers={}):
+        return NetworkRequest.post(cls._get_url('/users'), data, headers=headers)
 
     @classmethod
-    def get_user(cls, user_id):
-        return NetworkRequest.get(cls._get_url(f'/users/{user_id}'))
+    def get_user(cls, user_id, headers={}):
+        return NetworkRequest.get(cls._get_url(f'/users/{user_id}'), headers=headers)
 
     @classmethod
-    def login(cls, data):
-        return NetworkRequest.post(cls._get_url('/auth'), data)
+    def login(cls, data, headers={}):
+        return NetworkRequest.post(cls._get_url('/auth'), data, headers=headers)
 
     @classmethod
-    def token_gen(cls, data):
-        return NetworkRequest.post(cls._get_url('/auth/token'), data)
+    def token_gen(cls, data, headers={}):
+        return NetworkRequest.post(cls._get_url('/auth/token'), data, headers=headers)
 
     @classmethod
-    def list_tweets(cls):
-        return NetworkRequest.get(cls._get_url('/tweets'))
+    def list_tweets(cls, headers={}):
+        return NetworkRequest.get(cls._get_url('/tweets'), headers=headers)
 
     @classmethod
-    def create_tweet(cls, data):
-        return NetworkRequest.post(cls._get_url('/tweets'), data)
+    def create_tweet(cls, data, headers={}):
+        return NetworkRequest.post(cls._get_url('/tweets'), data, headers=headers)
 
     @classmethod
-    def get_tweet(cls, tweet_id):
-        return NetworkRequest.get(cls._get_url(f'/tweets/{tweet_id}'))
+    def get_tweet(cls, tweet_id, headers={}):
+        return NetworkRequest.get(cls._get_url(f'/tweets/{tweet_id}'), headers=headers)
 
     @classmethod
-    def update_tweet(cls, tweet_id, data):
-        return NetworkRequest.put(cls._get_url(f'/tweets/{tweet_id}'), data)
+    def update_tweet(cls, tweet_id, data, headers={}):
+        return NetworkRequest.put(cls._get_url(f'/tweets/{tweet_id}'), data, headers=headers)
 
     @classmethod
-    def delete_tweet(cls, tweet_id):
-        return NetworkRequest.delete(cls._get_url(f'/tweets/{tweet_id}'))
+    def delete_tweet(cls, tweet_id, headers={}):
+        return NetworkRequest.delete(cls._get_url(f'/tweets/{tweet_id}'), headers=headers)
 
 class Auth:
     def __init__(self, username=None, password=None):
@@ -110,5 +110,47 @@ class Auth:
         except:
             self.access_token = None
             print('Error: Could not generate access token')
+    def __str__(self):
+        return f'Access Token: {self.access_token}, Refresh Token: {self.refresh_token}'
 
-    
+
+def retry_mechanism(func):
+    def inner(self,*args, **kwargs):
+        returnValue = func(self,*args, **kwargs)
+        if returnValue['code'] == 401:
+            self.auth.change_access_token()
+            return func(self,*args, **kwargs)
+        return returnValue
+    return inner
+
+class Twitter:
+    def __init__(self, username=None, password=None):
+        self.auth = Auth(username, password)
+
+    @retry_mechanism
+    def list_tweets(self):
+        return TwitterRequestHandler.list_tweets(headers={'Authorization': f'Bearer {self.auth.access_token}'})
+
+    @retry_mechanism
+    def create_tweet(self, data):
+        return TwitterRequestHandler.create_tweet(data, headers={'Authorization': f'Bearer {self.auth.access_token}'})
+
+    @retry_mechanism
+    def get_tweet(self, tweet_id):
+        return TwitterRequestHandler.get_tweet(tweet_id, headers={'Authorization': f'Bearer {self.auth.access_token}'})
+
+    @retry_mechanism
+    def update_tweet(self, tweet_id, data):
+        return TwitterRequestHandler.update_tweet(tweet_id, data, headers={'Authorization': f'Bearer {self.auth.access_token}'})
+
+    @retry_mechanism
+    def delete_tweet(self, tweet_id):
+        return TwitterRequestHandler.delete_tweet(tweet_id, headers={'Authorization': f'Bearer {self.auth.access_token}'})    
+
+
+
+print(TwitterRequestHandler.list_users())
+
+twitter = Twitter('adib', '1234')
+
+print(twitter.auth)
